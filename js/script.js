babea3a770cd0d750aa61f0a523e81fd03a66aa8
Vitor Otavio <vitor.otavio15@gmail.com>
2023-08-15 14:19:55 -0300

    Projeto concluido

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ function convertValues() {
   const CurrencyValueToConverted = document.querySelector(".currencyValue");
   const dolarToday = 5;
   const euroToday = 5.45;
+  const libraToday = 6.35;
+  const bitcoinToday = 145.956;
 
   if (currencySelect.value == "dolar") {
     CurrencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -23,6 +25,19 @@ function convertValues() {
       currency: "EUR",
     }).format(inputCurrencyValue / euroToday);
   }
+  if (currencySelect.value == "libra") {
+    CurrencyValueToConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency: "GBP",
+    }).format(inputCurrencyValue / libraToday);
+  }
+  if (currencySelect.value == "bitcoin") {
+    CurrencyValueToConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BTC",
+      minimumFractionDigits: 8,
+    }).format(inputCurrencyValue / bitcoinToday);
+  }
 
   CurrencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
@@ -30,5 +45,30 @@ function convertValues() {
   }).format(inputCurrencyValue);
 
 }
+
+function changeCurrency(){
+    const currencyName = document.getElementById('currencyName');
+    const currencyImg = document.querySelector('.currencyImg');
+
+    if(currencySelect.value == 'dolar') {
+      currencyName.innerHTML = 'Dolar americano';
+      currencyImg.src = './assets/dolar.png'
+    }
+    if(currencySelect.value == 'euro') {
+      currencyName.innerHTML = 'Euro'
+      currencyImg.src = './assets/euro.png'
+    }
+    if(currencySelect.value == 'libra') {
+      currencyName.innerHTML = 'Libra'
+      currencyImg.src = './assets/libra.png'
+    }
+    if(currencySelect.value == 'bitcoin') {
+      currencyName.innerHTML = 'Bitcoin'
+      currencyImg.src = './assets/bitcoin.png'
+    }
+    convertValues()
+}
+
+currencySelect.addEventListener('change', changeCurrency);
 
 convertButton.addEventListener("click", convertValues);
